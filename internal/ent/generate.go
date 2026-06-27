32e7d44d20be4b4909441e2c56e5eb8d4733e13f
tdakkota <github@tdakkota.dev>
2026-06-27 19:11:43 +0300

    chore: commit generated files

---
+++ internal/ent/generate.go
@@ -0,0 +1,4 @@
+// Package ent provides database entity and ORM abstractions.
+package ent
+
+//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate ./schema --feature sql/upsert
---
+++ internal/oas/generate.go
@@ -0,0 +1,4 @@
+// Package oas contains the generated OpenAPI server implementation.
+package oas
+
+//go:generate go run -mod=mod github.com/ogen-go/ogen/cmd/ogen --target . --package oas --clean ../../api/openapi.yaml
